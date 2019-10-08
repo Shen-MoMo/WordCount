875d2e15fcbfc873d79080c538f4c67c3f68855e
Shen-MoMo <644245985@qq.com>
2019-10-08 15:52:19 +0800

    添加了代码规范，填写了PSP表格
    项目内使用注释描述了代码规范，填写了PSP表格中的预估时间

--- wordCount/wordCount/Program.cs
+++ wordCount/wordCount/Program.cs
@@ -3,7 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*
+ * 代码规范:
+ * （驼峰命名法）第一个单词首字母小写，后面其他单词首字母大写。
+ * 规定：  1.尽可能使用英文命名
+ *         2.不使用缩写
+ *         3.使用TAB作为缩进，缩进大小为4
+ *         4.类型名称和源文件名称一致
+ *         5.调用类型成员内部其他成员，需加this,调用父类成员需加base
+ * 参考博客：https://www.cnblogs.com/AaronBlogs/p/6815848.html
+ */
 namespace wordCount
 {
     class Program
