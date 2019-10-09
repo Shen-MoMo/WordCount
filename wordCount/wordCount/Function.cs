7829ee3a26ae01a56c14dd9493eafcb0a4c80e0a
Shen-MoMo <644245985@qq.com>
2019-10-09 17:40:44 +0800

    重新整理了代码

--- wordCount/wordCount/Function.cs
+++ wordCount/wordCount/Function.cs
@@ -3,18 +3,109 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Collections;
+
 
 namespace wordCount
 {
     class Function
     {
-        static int countCharacter(string sentence)
+        public static int getChacactor(string fileName)
         {
-            int count = 0;
-            
-
-         
+            //统计字符数的方法 
+            int i, count;
+            count = 0;
+            //打开文件
+            StreamReader fs = new StreamReader(fileName);
+            string str = null;
+            while ((str = fs.ReadLine()) != null)
+            {
+                for (i = 0; i < str.Length; i++)
+                {
+                    if (str[i] >= 0 && str[i] <= 127)
+                    {
+                        // Console.Write(str[i]);
+                        count++;
+                    }
+                }
+            }
+            fs.Close();
+            Console.Write("字符统计成功!\n");
+            Console.Write("characters：" + count + "\n");
             return count;
+        }
+        public static int getRows(string fileName)
+        {
+            //统计行数的方法         
+            FileStream fs = new FileStream(fileName, FileMode.Open);//打开文件
+            StreamReader sr = new StreamReader(fs, Encoding.Default);//用特定方式读取文件中信息
+            string s = sr.ReadToEnd();//读出所有信息
+            fs.Close();
+            sr.Close();
+            char[] c = { '\n' };//定义跳过的字符类型，换行符
+            string[] words = s.Split(c, StringSplitOptions.RemoveEmptyEntries);//将读出的信息按跳过的字符类型，分割成字符串
+            Console.Write("行数统计成功\n");
+            Console.Write("lines：" + words.Length + "\n");
+            return words.Length;//返回字符串的个数,即行数          
+        }
+        public static int totalWord(string fileName)
+        {
+            //统计单词的总数
+            FileStream fs = new FileStream(fileName, FileMode.Open);//打开文件
+            StreamReader sr = new StreamReader(fs, Encoding.Default);
+            string s = sr.ReadToEnd();//读取所有信息
+            Regex rg = new Regex("[A-Za-z-]+");//用正则表达式匹配单词
+            MatchCollection mc;//通过正则表达式所找到的成功匹配的集合
+            mc = rg.Matches(s);
+            fs.Close();
+            sr.Close();
+            Console.Write("words:" + mc.Count + "\n");
+            return mc.Count;
+        }
+        public static Hashtable countWord(string fileName)
+        {
+            //统计单个单词的出现次数
+            FileStream fs = new FileStream(fileName, FileMode.Open);//打开文件
+            StreamReader sr = new StreamReader(fs, Encoding.Default);
+            string s = sr.ReadToEnd();//读取所有信息
+
+            Regex rg = new Regex("[A-Za-z-]+");//用正则表达式匹配单词
+            MatchCollection mc;//通过正则表达式所找到的成功匹配的集合
+            mc = rg.Matches(s);
+            Hashtable wordList = new Hashtable();
+            for (int i = 0; i < mc.Count; i++)
+            {
+                string mcTmp = mc[i].Value.ToLower();//大小写不敏感
+                if (mcTmp.Length >= 4)
+                {
+                    if (!wordList.ContainsKey(mcTmp))//第一次出现的单词添加为key
+                    {
+                        wordList.Add(mcTmp, 1);
+                    }
+                    else
+                    {
+                        int value = (int)wordList[mcTmp];
+                        value++;
+                        wordList[mcTmp] = value;
+                    }
+                }
+            }
+            fs.Close();
+            sr.Close();
+            string[] keys = new string[wordList.Count];
+            int[] values = new int[wordList.Count];
+            wordList.Keys.CopyTo(keys, 0);
+            wordList.Values.CopyTo(values, 0);
+            Array.Sort(values, keys);
+            Array.Reverse(keys);
+            for (int j = 0; j < 10; j++)
+            {
+                Console.WriteLine(keys[j] + ":" + wordList[keys[j]]);
+            }
+            return wordList;
         }
     }
 }
--- wordCount/wordCount/Program.cs
+++ wordCount/wordCount/Program.cs
@@ -22,10 +22,10 @@ namespace wordCount
 {
     class Program
     {
-        public static string[] Information = { "", "", "" };//定义写入文件的3种信息
+        public static string[] Information = { "", "", "" ,""};//定义写入文件的3种信息
         public static void Main(string[] args)
         {
-            string fileName = Environment.CurrentDirectory + "\\file.txt";//被执行的文件        
+            string fileName = Environment.CurrentDirectory + "\\file.txt";//执行DEBUG目录下的文件        
             Console.Write("wordCount.exe ");
 
             string message = Console.ReadLine();//读取写入的各种操作符（-c -w -l -o）,顺序可颠倒
@@ -38,37 +38,23 @@ namespace wordCount
                 //对相应的命令执行相应的操作，结果写入returnNumber
                 if (s[i] == "-c")
                 {
-                    returnNumber[i] = getChacactor(fileName);
-                    Console.Write("characters：" + returnNumber[i] + "\n");
+                    returnNumber[i] = Function.getChacactor(fileName);
                     Information[i] = "characters：：" + returnNumber[i] + "\n";
                 }
                 else if (s[i] == "-l")
                 {
-                    returnNumber[i] = getRows(fileName);
-                    Console.Write("lines：" + returnNumber[i] + "\n");
+                    returnNumber[i] = Function.getRows(fileName);
                     Information[i] = "lines：" + returnNumber[i] + "\n";
                 }
                 else if (s[i] == "-n")
                 {
-                    returnNumber[i] = totalWord(fileName);
-                    Console.Write("words:" + returnNumber[i] + "\n");
+                    returnNumber[i] = Function.totalWord(fileName);
                     Information[i] = "words:" + returnNumber[i] + "\n";
                 }
                 else if(s[i] == "-w")
                 {
-                    Hashtable returnTable = countWord(fileName);
-                    //ArrayList wordList = new ArrayList(returnTable.Keys);
-                    //wordList.Sort();
-                    string[] keys = new string[returnTable.Count];
-                    int[] values = new int[returnTable.Count];
-                    returnTable.Keys.CopyTo(keys, 0);
-                    returnTable.Values.CopyTo(values, 0);
-                    Array.Sort(values, keys);
-                    Array.Reverse(keys);
-                    for (int j = 0; j < 10; j++) 
-                    {
-                        Console.WriteLine(keys[j] + ":" + returnTable[keys[j]]);
-                    }                   
+                    Hashtable returnTable = Function.countWord(fileName);
+                             
                 }
                 else if (s[i] == "-o")
                 {
@@ -79,91 +65,14 @@ namespace wordCount
             Console.ReadLine();
             
         }
-        public static int getChacactor(string fileName)
-        {
-            //统计字符数的方法 
-            int i, count;
-            count = 0;                    
-            //打开文件
-            StreamReader fs = new StreamReader(fileName);
-            string str = null; 
-            while ((str = fs.ReadLine()) != null)
-            {
-                for (i = 0; i < str.Length; i++)
-                {
-                    if (str[i] >= 0 && str[i] <= 127)
-                    {
-                       // Console.Write(str[i]);
-                        count++;
-                    }
-                }
-            }
-            fs.Close();                                      
-            Console.Write("字符统计成功!\n");             
-            return count;
-        }
-        public static int getRows(string fileName)
-        {
-            //统计行数的方法         
-            FileStream fs = new FileStream(fileName, FileMode.Open);//打开文件
-            StreamReader sr = new StreamReader(fs, Encoding.Default);//用特定方式读取文件中信息
-            string s = sr.ReadToEnd();//读出所有信息
-            fs.Close();
-            sr.Close();
-            char[] c = { '\n' };//定义跳过的字符类型，换行符
-            string[] words = s.Split(c, StringSplitOptions.RemoveEmptyEntries);//将读出的信息按跳过的字符类型，分割成字符串
-            Console.Write("行数统计成功\n");
-            return words.Length;//返回字符串的个数,即行数          
-        }
-        public static int totalWord(string fileName)
-        {
-            //统计单词的总数
-            FileStream fs = new FileStream(fileName, FileMode.Open);//打开文件
-            StreamReader sr = new StreamReader(fs, Encoding.Default);
-            string s = sr.ReadToEnd();//读取所有信息
-            Regex rg = new Regex("[A-Za-z-]+");//用正则表达式匹配单词
-            MatchCollection mc;//通过正则表达式所找到的成功匹配的集合
-            mc = rg.Matches(s);
-                
-            return mc.Count;
-        }
-        public static Hashtable countWord(string fileName)
-        {
-            //统计单个单词的出现次数
-            FileStream fs = new FileStream(fileName, FileMode.Open);//打开文件
-            StreamReader sr = new StreamReader(fs, Encoding.Default);
-            string s = sr.ReadToEnd();//读取所有信息
 
-            Regex rg = new Regex("[A-Za-z-]+");//用正则表达式匹配单词
-            MatchCollection mc;//通过正则表达式所找到的成功匹配的集合
-            mc = rg.Matches(s);
-            Hashtable wordList = new Hashtable();
-            for (int i = 0; i < mc.Count; i++)
-            {
-                string mcTmp = mc[i].Value.ToLower();//大小写不敏感
-                if(mcTmp.Length>=4)
-                {
-                    if(!wordList.ContainsKey(mcTmp))//第一次出现的单词添加为key
-                    {
-                        wordList.Add(mcTmp,1);
-                    }
-                    else
-                    {
-                        int value = (int)wordList[mcTmp];
-                        value++;
-                        wordList[mcTmp] = value;
-                    }
-                }
-            }
-            return wordList;
-        }
         public static void save()
         {
             //写入文件的方法        
             string fileName = Environment.CurrentDirectory + "\\result.txt";//被执行的文件        
             FileStream fs = new FileStream(fileName, FileMode.Create);//定义文件操作类型,实例化
             StreamWriter sw = new StreamWriter(fs);//用特定方式写入信息,实例化
-            for (int i = 0; i < 2; i++)
+            for (int i = 0; i < 3; i++)
             {
                 sw.Write(Information[i]);//写入第i种信息
                 sw.Write("\r\n");//换行
